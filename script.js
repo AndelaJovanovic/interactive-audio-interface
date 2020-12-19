62d2f4c916bfcef1219bf6b1533f9f5bc820c7be
Andela Jovanovic <kendjelka@Andelas-MacBook-Pro.local>
2020-12-19 19:03:18 +0100

    sounds work on click, but still showing a 404

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 window.addEventListener("load", () => {
     const audios = document.querySelectorAll(".audio")
     const rectangles = document.querySelectorAll(".rectangles div")
-
+    const player = document.querySelector(".player")
+    const shades = [
+        "#70D6FF",
+        "#FF70A6",
+        "#FF9770",
+        "#FFD670",
+        "#E9FF70",
+        "#333533"
+    ]
     console.log(audios)
 
     rectangles.forEach((rectangle, item) => {
         rectangle.addEventListener("click", function() {
+            audios[item].currentTime = 0
             audios[item].play()
         })
     })
